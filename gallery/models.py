5e7870f24c6e8b9135c55537544f3d0373fb56fe
Vanili <katevanili98@gmail.com>
2022-05-27 23:07:15 +0300

    Installed dependencies

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Location(models.Model):
-    location = models.CharField(max_length=80, unique= True)
+    location_name = models.CharField(max_length=80, unique= True)
 
     class Meta:
         ordering = ["location"]
@@ -12,10 +12,17 @@ class Location(models.Model):
         verbose_name_plural = "Locations"
 
     def __str__(self):
-        return self.location
+        return self.location_name
+
+    def save_location(self):
+        self.save()
+        
+    
+    def delete_location(self):
+        self.delete()
 
 class Category(models.Model):
-    category = models.CharField(max_length=80, unique= True)
+    category_name = models.CharField(max_length=80, unique= True)
 
     class Meta:
         ordering = ["category"]
@@ -23,4 +30,45 @@ class Category(models.Model):
         verbose_name_plural = "Category"
 
     def __str__(self):
-        return self.category
+        return self.category_name
+
+    def save_category(self):
+        self.save()
+        
+    
+    def delete_category(self):
+        self.delete()
+
+class Image(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField()
+    photo = models.ImageField(upload_to = 'photos/')
+    category = models.ForeignKey(Category, related_name='category', on_delete=models.DO_NOTHING)
+    location = models.ForeignKey(Location, related_name='location', on_delete=models.DO_NOTHING)
+
+
+    def save_image(self):
+        self.save()
+    
+    
+    def delete_image(self):
+        self.delete()
+
+    def summary(self):
+        return self.description[:100]+" ......"
+    
+    def pub_date(self):
+        return self.pub_date.strftime('%b %e %Y')
+    
+    def update_image(self):
+        pass
+        
+    def get_image_by_id(cls):
+        images = cls.objects.get(pk=id)
+        return images
+    
+    def search_image(category):
+        pass
+    
+    def filter_by_location(location):
+        pass
