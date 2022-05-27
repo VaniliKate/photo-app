56771f83bd912b8a385983561514c77552709b7e
Vanili <katevanili98@gmail.com>
2022-05-27 19:31:34 +0300

    Created the Location model

--- gallery/models.py
+++ gallery/models.py
@@ -2,3 +2,16 @@ from unittest import mock
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    location = models.CharField(max_length=80, unique= True)
+
+    class Meta:
+        ordering = ["location"]
+        verbose_name = "Location"
+        verbose_name_plural = "Locations"
+
+    def __str__(self):
+        return self.name
+
+
