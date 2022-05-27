b8367b044c2b2e8580e38e8314462126a1f1c5c1
Vanili <katevanili98@gmail.com>
2022-05-28 00:07:25 +0300

    News changes

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,4 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+urlpatterns =[
+    path('', views.index, name = 'home'),
+    path('search/', views.search_category, name = 'gallery'),
+   
+]
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
+
+def search_category(request):
+
+    return render(request, 'search.html')
