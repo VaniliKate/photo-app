b49928509ec6253b38447a26d90881a8f1a8d4a3
Vanili <katevanili98@gmail.com>
2022-05-27 19:20:40 +0300

    Made the mygallery database and an initial migration

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,4 @@
+from unittest import mock
 from django.db import models
 
 # Create your models here.
--- photos/settings.py
+++ photos/settings.py
@@ -78,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME' : 'mygallery',
+        'USER' : 'vanili',
+        'PASSWORD' : 'Swift3030',
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
